29abe2d452b82c83dd8d9fb162ee9a3f3358b2af
Garden <kamil.ogrodowczyk111@gmail.com>
2021-01-05 16:04:48 +0100

    Complete sticky and hamburger menu

--- app.js
+++ app.js
@@ -27,63 +27,43 @@
 //         console.log(walk)
 // });
 
-let resizeTimer;
-window.addEventListener("resize", () => {
-    document.body.classList.add("resize-animation-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-      document.body.classList.remove("resize-animation-stopper");
-    }, 400);  
-});
+// let resizeTimer;
+// window.addEventListener("resize", () => {
+//     document.body.classList.add("resize-animation-stopper");
+//     clearTimeout(resizeTimer);
+//     resizeTimer = setTimeout(() => {
+//       document.body.classList.remove("resize-animation-stopper");
+//     }, 400);  
+// });
 
 const header = document.querySelector('.header');
-
+const image = document.querySelector('.header__image')
+const togglerCheckbox = document.querySelector('.toggler[type="checkbox"]');
 
 function stickyHeader() {
+        
     if(window.scrollY > header.offsetHeight) {
         document.body.style.paddingTop = `${header.offsetHeight}px`;
         header.classList.add('header-sticky');
     } else {
         document.body.style.paddingTop = 0;
         header.classList.remove('header-sticky');
-    };
+    }
 }
 
-window.addEventListener('scroll', stickyHeader);
+function hiddenOverflow() {
 
-const navbar = document.querySelector('.nav');
-const navbars = navbar.querySelectorAll('li');
-const image = document.querySelector('.header__image')
-const nav = document.querySelector('.nav__menu');
-
-function toggleNav() {
-    nav.classList.toggle('active');
-    opacityNav(image, 'filter', 'invert(0)', 'invert(1)', 300);
-    opacityNav(navbars, 'opacity', 0, 1, 500);
-}
-
-let opacityTimer;
-function opacityNav(bar, attribute, startResult, endResult, time) {
-    
-
-    if(nav.classList.contains('active')) {
-        opacityTimer = setTimeout(() => {
-            if(bar.length > 1) {
-                bar.forEach(element => element.style[attribute] = endResult);
-            } else {
-                bar.style[attribute] = endResult;
-            }
-        }, time);
+    if(this.checked) {
+        document.body.style.overflowY = 'hidden';
+        image.classList.add('filtred');
     } else {
-        if(bar.length > 1) {
-            bar.forEach(element => element.style[attribute] = startResult);
-        } else {
-            bar.style[attribute] = startResult;
-        }
-        clearTimeout(opacityTimer);
-    }    
+        document.body.style.overflowY = 'scroll';
+        image.classList.remove('filtred');
+        stickyHeader();
+    }
 }
 
-navbar.addEventListener('click', toggleNav)
+togglerCheckbox.addEventListener('click', hiddenOverflow);
+window.addEventListener('scroll', stickyHeader);
 
 
