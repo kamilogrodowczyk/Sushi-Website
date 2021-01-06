4ff7e9c213d32d4dc69db5c4db5e21a5084721df
Garden <kamil.ogrodowczyk111@gmail.com>
2021-01-06 18:43:11 +0100

    Add map to the website

--- app.js
+++ app.js
@@ -27,14 +27,36 @@
 //         console.log(walk)
 // });
 
-// let resizeTimer;
-// window.addEventListener("resize", () => {
-//     document.body.classList.add("resize-animation-stopper");
-//     clearTimeout(resizeTimer);
-//     resizeTimer = setTimeout(() => {
-//       document.body.classList.remove("resize-animation-stopper");
-//     }, 400);  
-// });
+var mymap = L.map('map', { scrollWheelZoom: false }).setView([52.28593654278595, 16.85675418654572], 19);
+
+L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=lQ3MzVJ2ft3u1MukWcCe', {
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    maxZoom: 28,
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+var markerIcon = L.icon ({
+    iconUrl: 'img/marker.png',
+    iconSize: [42, 50],
+    iconAnchor: [20, 49],
+    popupAnchor: [0, -50]
+})
+
+var marker = L.marker([52.28593654278595, 16.85675418654572], {icon: markerIcon}).addTo(mymap);
+
+
+marker.bindPopup('Katei Sushi <br/> ul. Rynek 11, Puszczykowo');
+
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove("resize-animation-stopper");
+    }, 400);  
+});
 
 const header = document.querySelector('.header');
 const image = document.querySelector('.header__image')
